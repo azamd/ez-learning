5e0c335b4959ece535e1643b88c6834c207b5a0d
azamd <tinkertailor1999@gmail.com>
2023-09-11 19:55:06 +0100

    updates on e2e testing

--- src/test/java/com/ezlearning/platform/E2eAutomatedTest.java
+++ src/test/java/com/ezlearning/platform/E2eAutomatedTest.java
@@ -32,84 +32,84 @@ public class E2eAutomatedTest {
     @Order(2)
     public void testBrowseLinks() throws InterruptedException {
         setUp();
-        String[] links = {"/index", "/discover", "/cursos"};
+        String[] links = {"/index", "/discover", "/cursos", "/register"};
         for (String link : links) {
-            driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com" + link);
+            //driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com" + link);
+            driver.get("http://localhost:8080" + link);
+
 
         }
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     public void testRegister(){
-     setUp();
-     driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/register");
+    //driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/register");
+    driver.get("http://localhost:8080/register");
 
-   
-     WebElement name=driver.findElement(By.id("nombre"));
-     WebElement lastname=driver.findElement(By.id("apellido"));
-     WebElement email=driver.findElement(By.id("email"));
-     WebElement imgurl=driver.findElement(By.id("imgurl"));
-     WebElement username=driver.findElement(By.id("username"));
-     WebElement password=driver.findElement(By.id("password"));
-     WebElement registerbtn=driver.findElement(By.id("register"));
+    WebElement name=driver.findElement(By.id("nombre"));
+    WebElement lastname=driver.findElement(By.id("apellido"));
+    WebElement email=driver.findElement(By.id("email"));
+    WebElement imgurl=driver.findElement(By.id("imgurl"));
+    WebElement username=driver.findElement(By.id("username"));
+    WebElement password=driver.findElement(By.id("password"));
+    WebElement registerbtn=driver.findElement(By.id("register"));
 
-     name.sendKeys("George");
-     lastname.sendKeys("Wilson");
-     email.sendKeys("george.wilson@gmail.com");
-     imgurl.sendKeys("https://i.imgur.com/T0n0PH9.jpg");
-     username.sendKeys("GeorgeWilson01");
-     password.sendKeys("test");
+    name.sendKeys("George");
+    lastname.sendKeys("Wilson");
+    email.sendKeys("george.wilson@gmail.com");
+    imgurl.sendKeys("https://i.imgur.com/T0n0PH9.jpg");
+    username.sendKeys("GeorgeWilson01");
+    password.sendKeys("test");
 
-     registerbtn.click();
+    registerbtn.click();
 
-     String actualUrl="https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/register";
-
-     String expectedUrl= driver.getCurrentUrl();
-     Assert.assertEquals(expectedUrl,actualUrl);
-    }
-
-
-    @Test
-    @Order(4)
-    public void testLogin(){
-
-     driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/login");
-
-     WebElement username=driver.findElement(By.id("username"));
-     WebElement password=driver.findElement(By.id("password"));
-     WebElement login=driver.findElement(By.name("submit"));
-
-     username.sendKeys("GeorgeWilson01");
-     password.sendKeys("test");
-     login.click();
-
-     String actualUrl="https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/";
-
-     String expectedUrl= driver.getCurrentUrl();
-     Assert.assertEquals(expectedUrl,actualUrl);
-    } 
-
-
-    @Test
-    @Order(5)
-    public void testListProfessors(){
-
-    driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/profesores");
-
-    WebElement chevron_right=driver.findElement(By.id("chevron_right"));
-    chevron_right.click();
-    WebElement curso=driver.findElement(By.id("curso"));
-    curso.click();
+     //String actualUrl="https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/register";
+    
+    driver.get("http://localhost:8080/profesores");
     
     }
 
-   @Test
-   @Order(6)
-   public void testLogout(){
+    @Test
+    @Order(5)
+    public void testLogin(){
+    
+    testRegister();
+    driver.get("http://localhost:8080/login");
+    //driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/login");
 
-    driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/logout-success");
- }
+
+    WebElement username1=driver.findElement(By.id("username"));
+    WebElement password1=driver.findElement(By.id("password"));
+    WebElement login=driver.findElement(By.name("submit"));
+
+    username1.sendKeys("GeorgeWilson01");
+    password1.sendKeys("test");
+    login.click();
+
+     //String actualUrl="https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/";
+    String actualUrl1="http://localhost:8080/";
+
+
+    String expectedUrl1= driver.getCurrentUrl();
+    Assert.assertEquals(expectedUrl1,actualUrl1);
+
+    }
+
+    
+    @Test
+    @Order(4)
+    public void testProfessorDetail() throws InterruptedException{
+
+    //driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/profesores");
+
+    WebElement chevron_right=driver.findElement(By.id("chevron_right"));
+    chevron_right.click();
+
+    driver.get("http://localhost:8080/cursos/1");
+
+    }
+
 
 
 
