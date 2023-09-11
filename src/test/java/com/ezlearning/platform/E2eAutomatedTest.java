823db817cd2d4ad7b744e2d2b70af068b16278d6
azamd <tinkertailor1999@gmail.com>
2023-09-11 14:25:52 +0100

    testRegister in E2eAutomatedTest.java

--- src/test/java/com/ezlearning/platform/E2eAutomatedTest.java
+++ src/test/java/com/ezlearning/platform/E2eAutomatedTest.java
@@ -3,23 +3,24 @@ package com.ezlearning.platform;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-@RunWith(JUnit4.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class E2eAutomatedTest {
 
     private static ChromeDriver driver;
 
     @Test
+    @Order(1)
     public void setUp() {
         
         WebDriverManager.chromedriver().setup();
@@ -27,32 +28,62 @@ public class E2eAutomatedTest {
         driver.manage().window().maximize();
     }
 
-
     @Test
+    @Order(2)
     public void testBrowseLinks() throws InterruptedException {
         setUp();
         String[] links = {"/index", "/discover", "/cursos"};
         for (String link : links) {
             driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com" + link);
-            //driver.get("http://localhost:8080" + link);
 
         }
     }
 
     @Test
+    @Order(2)
+    public void testRegister(){
+     setUp();
+     driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/register");
+
+   
+     WebElement name=driver.findElement(By.id("nombre"));
+     WebElement lastname=driver.findElement(By.id("apellido"));
+     WebElement email=driver.findElement(By.id("email"));
+     WebElement imgurl=driver.findElement(By.id("imgurl"));
+     WebElement username=driver.findElement(By.id("username"));
+     WebElement password=driver.findElement(By.id("password"));
+     WebElement registerbtn=driver.findElement(By.id("register"));
+
+     name.sendKeys("George");
+     lastname.sendKeys("Wilson");
+     email.sendKeys("george.wilson@gmail.com");
+     imgurl.sendKeys("https://i.imgur.com/T0n0PH9.jpg");
+     username.sendKeys("GeorgeWilson01");
+     password.sendKeys("test");
+
+     registerbtn.click();
+
+     String actualUrl="https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/register";
+
+     String expectedUrl= driver.getCurrentUrl();
+     Assert.assertEquals(expectedUrl,actualUrl);
+    }
+
+
+    @Test
+    @Order(4)
     public void testLogin(){
 
      driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/login");
-     //driver.get("http://localhost:8080/login");
 
      WebElement username=driver.findElement(By.id("username"));
      WebElement password=driver.findElement(By.id("password"));
      WebElement login=driver.findElement(By.name("submit"));
-     username.sendKeys("test");
+
+     username.sendKeys("GeorgeWilson01");
      password.sendKeys("test");
      login.click();
 
-     //String actualUrl="http://localhost:8080/";
      String actualUrl="https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/";
 
      String expectedUrl= driver.getCurrentUrl();
@@ -60,25 +91,25 @@ public class E2eAutomatedTest {
     } 
 
 
-@Test
+    @Test
+    @Order(5)
     public void testListProfessors(){
 
-    //driver.get("http://localhost:8080/profesores");
     driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/profesores");
 
     WebElement chevron_right=driver.findElement(By.id("chevron_right"));
     chevron_right.click();
     WebElement curso=driver.findElement(By.id("curso"));
     curso.click();
-    WebElement mtr=driver.findElement(By.id("modal-matricula"));
-    mtr.click();
-    driver.findElement(By.name("modal"));
-    driver.findElement(By.name("modal-footer"));
-    driver.findElement(By.name("confirmar")).click();
-    WebElement confirm=driver.findElement(By.name("confirmar"));
-    confirm.click();
-     
+    
     }
+
+   @Test
+   @Order(6)
+   public void testLogout(){
+
+    driver.get("https://ez-learning-git-amd-med-aziz-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/logout-success");
+ }
 
 
 
